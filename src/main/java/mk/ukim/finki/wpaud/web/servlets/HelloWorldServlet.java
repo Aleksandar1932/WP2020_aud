982152a8882f2b70aecbdef18beade3a5a4238b6
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2020-10-22 11:07:53 +0200

    Add Exercise 1
    Hello world app

---
+++ src/main/java/mk/ukim/finki/wpaud/web/servlets/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package mk.ukim.finki.wpaud.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        PrintWriter out = resp.getWriter();
+        out.write("<html><head></head><body><h1>Hello world " + username + "</h1></body</html>");
+        out.flush();
+    }
+}
---
+++ src/test/java/mk/ukim/finki/wpaud/WpAudApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpaud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpAudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
