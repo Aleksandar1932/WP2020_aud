dc2d745df68a10644f452b10c169ee022fc3c209
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2020-12-18 19:36:11 +0100

    Add Exercise 9
    Security UI constraints

--- src/main/java/mk/ukim/finki/wpaud/config/WebSecurityConfig.java
+++ src/main/java/mk/ukim/finki/wpaud/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/assets/**", "/register").permitAll()
+                .antMatchers("/", "/home", "/assets/**", "/register","/products").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
